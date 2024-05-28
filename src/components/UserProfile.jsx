12c7fd46604031c309412120c3156b52f3346c14
Dennis Muturi <denismutur@gmail.com>
2024-05-28 13:02:30 +0300

    refactor: Add navigation button to go back in UserProfile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,7 +2,6 @@ import {
   collection,
   where,
   query,
-  getDocs,
   onSnapshot,
 } from "firebase/firestore";
 import { db, auth } from "@/config/firebase";
@@ -12,12 +11,15 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { FaUserCircle } from "react-icons/fa";
 import { Button } from "./ui/button";
 import { onAuthStateChanged } from "firebase/auth";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import MyTweets from "./MyTweets";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const UserProfile = () => {
   const [userDetails, setUserDetails] = useState({});
   const {state}=useLocation();
+  const navigate=useNavigate();
   console.log(state);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -36,6 +38,25 @@ const UserProfile = () => {
   }, []);
   return (
     <div>
+      <div className="flex">
+        <Button variant='outline' onClick={()=>{
+          navigate(-1);
+        }}>
+          <ArrowLeft />
+        </Button>
+        <Card className=" flex justify-center items-center w-fit">
+          <Avatar>
+            <AvatarImage src={userDetails.profilePicUrl} />
+            <AvatarFallback>
+              <FaUserCircle />
+            </AvatarFallback>
+          </Avatar>
+          @{userDetails.username}
+        </Card>
+      </div>
+        <h1 className="text-lg font-bold text-center">
+          {`${userDetails.username}`} profile
+        </h1>
       <Card>
         <CardHeader>
           <CardTitle className="flex">
@@ -49,15 +70,36 @@ const UserProfile = () => {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <span className="font-medium mr-2 text-xl">
-            {userDetails.followersCount} Followers
-          </span>
-          <span className="font-medium text-xl">
-            {userDetails.followingCount} Following
-          </span>
+          <Button variant="link" asChild className="text-lg">
+            <Link
+              to="/home/followersAndFollowing"
+              state={{
+                isFollowers: true,
+                userId: userDetails.userId,
+                username: userDetails.username,
+              }}
+            >
+              {userDetails.followersCount} Followers
+            </Link>
+          </Button>
+          <Button variant="link" asChild className="text-lg">
+            <Link
+              to="/home/followersAndFollowing"
+              state={{
+                isFollowers: false,
+                userId: userDetails.userId,
+                username: userDetails.username,
+              }}
+            >
+              {userDetails.followingCount} Following
+            </Link>
+          </Button>
         </CardContent>
       </Card>
-      <MyTweets userId={state.userId}/>
+      <h1 className="text-lg font-bold text-center">
+        {`${userDetails.username}'s `}Tweets
+      </h1>
+      <MyTweets userId={state.userId} />
     </div>
   );
 };
